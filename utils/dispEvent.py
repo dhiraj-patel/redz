11ef1cc594d8c97ce1640ce76616c875f3bcea05
esandine <esandine@stuy.edu>
2016-12-06 20:19:43 -0500

    Finished dispEvent.py

--- utils/dispEvent.py
+++ utils/dispEvent.py
@@ -24,6 +24,13 @@ def searchEvents(zip, range, year, month, day, hour, minute):
         i+=1
     return ret
 
+#seachEvents(zip, range, year, month, day, hour, minute)
+#Params:
+# events - list of events
+# budget - int budget in dollars
+#Returns: api url to next event
+#What it does: removes the first event from the list and checks the price, if
+#it is below the cost it returns the event, if not it recursively calls itself
 def nextEvent(events,budget):
     q = events[0]
     events.remove(q)
@@ -34,15 +41,33 @@ def nextEvent(events,budget):
     data = json.loads( response )
     isfree = data['ticket_classes'][0]['free']
     if(isfree):
-        return data['resource_uri']
+        return data['resource_uri']+"?token=COVN2QEFIDLBA54TVAVS&expand=ticket_classes"
     elif(data['ticket_classes'][0]['cost']['value']/100<budget):
-        return data['resource_uri']
+        return data['resource_uri']+"?token=COVN2QEFIDLBA54TVAVS&expand=ticket_classes"
     else:
         return nextEvent(events,budget)
-    
+
+#dispEvent(event)
+#Params:
+# event - event url
+#Returns: dictionary with all of the data for the event
+#What it does: grabs data from api
+def dispEvent(event):
+    u = urllib2.urlopen(event+"&expand=venue,logo_id")
+    response = u.read()
+    data = json.loads( response )
+    d = {}
+    d['name']=data['name']['text']
+    d['description']=data['description']['text']
+    d['time']=data['start']['local']
+    d['place']=data['venue']['name']
+    d['address']=data['venue']['address']['localized_address_display']
+    d['logo']=data['logo']['url']
+    d['url']=data['url']
+    return d
+
+
 if __name__ == "__main__":
     events = searchEvents(11215, 20, 2016, 12, 15, 12, 30)
-    print nextEvent(events, 100)
-    print nextEvent(events, 100)
-    print nextEvent(events, 100)
+    print dispEvent(nextEvent(events, 100))
                           

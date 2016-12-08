0f3a0429b2d04f58581f12341ea9a62864d6cd70
esandine <esandine@stuy.edu>
2016-12-08 18:12:38 -0500

    added nextFood

--- utils/dispFood.py
+++ utils/dispFood.py
@@ -18,31 +18,22 @@ def searchFood(lat, long, rad, maxprice):
     u = urllib2.urlopen(q)
     response = u.read()
     data = json.loads( response )
-    print data["results"][0]
     return data
 
-#nextEvent(events,budget)
+#nextFood(events)
 #Params:
 # events - list of events
-# budget - int budget in dollars
 #Returns: api url to next event
 #What it does: removes the first event from the list and checks the price, if
 #it is below the cost it returns the event, if not it recursively calls itself
-'''def nextEvent(events,budget):
-    q = events[0]
-    events.remove(q)
-    q+="?token=COVN2QEFIDLBA54TVAVS"
-    q+="&expand=ticket_classes"
-    u = urllib2.urlopen(q)
-    response = u.read()
-    data = json.loads( response )
-    isfree = data['ticket_classes'][0]['free']
-    if(isfree):
-        return data['resource_uri']+"?token=COVN2QEFIDLBA54TVAVS&expand=ticket_classes"
-    elif(data['ticket_classes'][0]['cost']['value']/100<budget):
-        return data['resource_uri']+"?token=COVN2QEFIDLBA54TVAVS&expand=ticket_classes"
-    else:
-        return nextEvent(events,budget)'''
+def nextFood(foods):
+    food = foods["results"][0];
+    q = "https://maps.googleapis.com/maps/api/place/details/json?"
+    q+="key=AIzaSyADc7Kdirb61v6g5LBZdisoLLeG3q_j03g"
+    q+= "&placeid="+str(food["id"])
+    foods["results"].remove(food)
+    return q
 
 
-searchFood(40.6653873,-73.9861546,1000,4)
+foods=searchFood(40.6653873,-73.9861546,1000,4)
+print nextFood(foods)

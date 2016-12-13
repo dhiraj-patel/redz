57bf8cc4622537e0c3e7df3654e0d72b5b12a1d2
oreo21 <reok99@gmail.com>
2016-12-13 18:22:41 -0500

    summary.html displays plan information

--- app.py
+++ app.py
@@ -7,88 +7,111 @@ app.secret_key = 'nine'
 @app.route('/')
 @app.route('/login')
 def login():
-    if 'user' in session:
-        return redirect(url_for('home'))
-    return render_template('login.html')
+	if 'user' in session:
+		return redirect(url_for('home'))
+	return render_template('login.html')
 
 @app.route('/home')
 def home():
-    if 'user' in session:
-        mHome = "Welcome back " + session['user'] + "!"
-        return render_template('home.html', messageHome = mHome)
-    else:
-        return redirect(url_for('login'))
+	if 'user' in session:
+		mHome = "Welcome back " + session['user'] + "!"
+		return render_template('home.html', messageHome = mHome)
+	else:
+		return redirect(url_for('login'))
 
 @app.route("/authenticate", methods=['POST'])
 def authenticate():
-    u = request.form['username']
-    p = request.form['password']
-    a = request.form['action']
-    data = auth.authenticate([u, p, a])
-    if data[1]: #['message', boolean]
-        session['user'] = u
-        return redirect(url_for('home'))
-    else:
-        return render_template('login.html', messageLogin = data[0])
+	u = request.form['username']
+	p = request.form['password']
+	a = request.form['action']
+	data = auth.authenticate([u, p, a])
+	if data[1]: #['message', boolean]
+		session['user'] = u
+		return redirect(url_for('home'))
+	else:
+		return render_template('login.html', messageLogin = data[0])
 
 @app.route('/logout', methods=['POST'])
 def logout():
-    if 'user' in session:
-        session.pop('user')
-    return redirect(url_for('login'))
+	if 'user' in session:
+		session.pop('user')
+	return redirect(url_for('login'))
 
 
 @app.route('/findEvents')
 def find():
-    return render_template('newEvent.html')
-        
+	return render_template('newEvent.html')
+		
 
 @app.route('/viewEvent', methods=["POST"])
 def outputResults():
-    formzip = request.form["Zip Code"]
-    budgetrange = request.form["Budget"]
-    distance = request.form["Radius"]
-    time = request.form["Time"]
-    date = request.form["Date"]
-    
-    timex = time.split(':')
-    datex = date.split('-')
+	formzip = request.form["Zip Code"]
+	budgetrange = request.form["Budget"]
+	distance = request.form["Radius"]
+	time = request.form["Time"]
+	date = request.form["Date"]
+	
+	timex = time.split(':')
+	datex = date.split('-')
 
-    year = int(datex[0])
-    month = int(datex[1])
-    day = int(datex[2])
-    hour = int(timex[0])
-    minute = int(timex[1])
-    if year < 2015:
-        return render_template('newEvent.html',errorMessage="Bad Request For Year")
+	year = int(datex[0])
+	month = int(datex[1])
+	day = int(datex[2])
+	hour = int(timex[0])
+	minute = int(timex[1])
+	if year < 2015:
+		return render_template('newEvent.html',errorMessage="Bad Request For Year")
 
-    else: 
-        events = dispEvent.searchEvents(formzip,distance,year,month,day,hour,minute)
-        i = 0
-        allEvents = []
-        while i<5: 
-            d=dispEvent.dispEventResults(dispEvent.nextEvent(events,budgetrange))
-            allEvents.append(d)
-            i+=1
-        return render_template('listEvent.html', eventpar = allEvents, number = i)
-    
+	else: 
+		events = dispEvent.searchEvents(formzip,distance,year,month,day,hour,minute)
+		i = 0
+		allEvents = []
+		while i < 5: 
+			d=dispEvent.dispEventResults(dispEvent.nextEvent(events,budgetrange))
+			allEvents.append(d)
+			i+=1
+		return render_template('listEvent.html', eventpar = allEvents)
+	
 @app.route('/viewFoods',methods=["POST"])
 def foodResults():
-    formlat = request.form['lat']
-    formlong = request.form['long']
-    formbudget = 4
-    formRadius = 1609 #1 mile in meters
+	formlat = request.form['lat']
+	formlong = request.form['long']
+	formbudget = 4
+	formRadius = 1609 #1 mile in meters
 
-    i = 0
-    allFoods = []
-    foodFound = dispFood.searchFood(formlat,formlong,formRadius,formbudget)
-    while i<len(foodFound["results"]):
-        f = dispFood.dispFoodResults(dispFood.nextFood(foodFound,i))
-        allFoods.append(f)
-        i+=1
-    return render_template('listFood.html', foodpar = allFoods, number=i)
-    
+	eList = []
+	eName = request.form['eName']
+	eDesc = request.form['eDesc']
+	eTime = request.form['eTime']
+	eVenue = request.form['eVenue']
+	eAddress = request.form['eAddress']
+	eList.extend([eName, eDesc, eTime, eVenue, eAddress])
+	#print '\n'.join(eList)
+
+	i = 0
+	allFoods = []
+	foodFound = dispFood.searchFood(formlat,formlong,formRadius,formbudget)
+	while i<len(foodFound["results"]):
+		f = dispFood.dispFoodResults(dispFood.nextFood(foodFound,i))
+		allFoods.append(f)
+		i+=1
+	return render_template('listFood.html', foodpar = allFoods, eventData = eList)
+
+
+@app.route('/summary', methods = ['POST'])
+def dispSummary():
+	efList = []
+	eName = request.form['eName']
+	eDesc = request.form['eDesc']
+	eTime = request.form['eTime']
+	eVenue = request.form['eVenue']
+	eAddress = request.form['eAddress']
+	fName = request.form['fName']
+	fAddress = request.form['fAddress']
+	efList.extend([eName, eDesc, eTime, eVenue, eAddress, fName, fAddress])
+	return render_template('summary.html', planData = efList)
+	
 
 if __name__ == "__main__":
-    app.debug = True
-    app.run()
+	app.debug = True
+	app.run()
--- utils/placesAPI.py
+++
@@ -1,8 +0,0 @@
-import urllib2, json
-
-def getInfo():
-    u = urllib2.urlopen(https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&type=restaurant&keyword=cruise&key=AIzaSyADc7Kdirb61v6g5LBZdisoLLeG3q_j03g)
-    r = u.read()
-    data = r.loads()
-    print data
-
